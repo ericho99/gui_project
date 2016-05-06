9ac8be493edceb93192d21a9368cc7b11d872449
Eric Ho <ericho42@gmail.com>
2016-05-06 03:03:48 -0400

    small changes

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
+from flask import Flask, render_template, url_for
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, World!"
+  return render_template('index.html')
