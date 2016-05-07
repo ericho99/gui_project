96a71552e4e0c708f4a1ec161b0992ea6c20df67
Eric Ho <ericho42@gmail.com>
2016-05-06 21:55:13 -0400

    initial structure of the app

--- app/views.py
+++ app/views.py
@@ -5,3 +5,8 @@ from flask import Flask, render_template, url_for
 @app.route('/index')
 def index():
   return render_template('index.html')
+
+@app.route('/collection')
+@app.route('/collection/<int:cid>')
+def collection(cid = 0):
+  return render_template('collection.html')
